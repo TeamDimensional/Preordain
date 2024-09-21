0351d9e9b191acf13ac589425c7fb0693190ad32
Wizzerinus <development@toontowneventhorizon.com>
2024-09-21 14:10:33 +0300

    changed base tags

--- src/main/java/com/teamdimensional/preordain/Preordain.java
+++ src/main/java/com/teamdimensional/preordain/Preordain.java
@@ -1,13 +1,12 @@
-package com.example.modid;
+package com.teamdimensional.preordain;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class Preordain {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
